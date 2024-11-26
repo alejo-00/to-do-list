645f8c1d63010d6eeb7ab50f643178856f83d141
alejo-00 <alejolescano3@gmail.com>
2024-11-26 15:48:26 -0300

    new function to delete items and changes in general code

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,52 @@ const counterList = document.getElementById("counter-list");
 const inptDescription = document.getElementById("inpt-description");
 const btnAdd = document.getElementById("btn-add");
 const listTodo = document.getElementById("list-todo");
-let count = 0;
 
-const itemAgree = () => {
+
+//FUNCTION TO ADD ITEMS WHEN CLICK IN ADD BUTTON
+const itemAdd = () => {
     if(validateDescription()){
         const itemDescription = inptDescription.value;
+        //NEW ITEM
         const newItem = document.createElement("li");
+        //NEW BOTON DELETE
         const newBtnDelete = document.createElement("button");
+        //NEW ICON DELETE
+        const newIconDelete = document.createElement("i");
         newItem.textContent = itemDescription;
-        listTodo.appendChild(newItem).appendChild(newBtnDelete);
-        count += 1;
-        counterList.textContent = count;
-    }
+        newItem.className = "col-md-4 d-flex justify-content-between";
+        newIconDelete.className = "bi bi-x-square";
+        newBtnDelete.appendChild(newIconDelete);
+        newItem.appendChild(newBtnDelete);
+        listTodo.appendChild(newItem);
+
+
+        //FUNCTION TO DELETE ITEMS WHEN CLICK IN DELETE BUTTON
+        newBtnDelete.addEventListener("click", (e) => {
+            e.preventDefault();
+            newItem.remove();
+            counterItems();
+        });
+        counterItems();
+    };
     clearInputs();
 };
 
+
+//FUNCTION TO COUNTER ITEMS WHEN IS ADD OR DELETE ITEMS
+const counterItems = () => {
+    let count = document.getElementsByTagName("li");
+    counterList.textContent = count.length;
+};
+
+
+//FUNCTION TO CLEAR INPUTS WHEN IS ADD DESCRIPTION I
 const clearInputs = () => {
     inptDescription.value = "";
 };
 
+
+//FUNCTION TO VALIDATE DESCRIPTION CONTENT CAN NOT BE EMPTY
 const validateDescription = () => {
     if(inptDescription.value == ""){
         alert("Error! a description must be entered");
@@ -33,10 +60,11 @@ const validateDescription = () => {
     
 };
 
+
+//READY
 document.addEventListener("DOMContentLoaded", function() {
     btnAdd.addEventListener("click", (e) => {
         e.preventDefault();
-        itemAgree();
-        // console.log(e);
-    })
+        itemAdd();
+    });
 });
