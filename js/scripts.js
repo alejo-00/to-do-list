3bf83d09d00ae15161bed46e274753e8ad6d1ba5
alejo-00 <alejolescano3@gmail.com>
2025-02-14 12:03:31 -0300

    implementing the editing function

--- js/scripts.js
+++ js/scripts.js
@@ -5,36 +5,73 @@ const btnAdd = document.getElementById("btn-add");
 const listTodo = document.getElementById("list-todo");
 
 
+let count = 0;
+
+
 //FUNCTION TO ADD ITEMS WHEN CLICK IN ADD BUTTON
 const itemAdd = () => {
     if(validateDescription()){
-        const itemDescription = inptDescription.value;
         //NEW ITEM
         const newItem = document.createElement("li");
+        newItem.id = `item${count}`
+        count += 1;
+
         //NEW BOTON DELETE
         const newBtnDelete = document.createElement("button");
+
         //NEW ICON DELETE
         const newIconDelete = document.createElement("i");
+
         //NEW BOTON EDIT
         const newBtnEdit = document.createElement("button");
+
         //NEW ICON EDIT
         const newIconEdit = document.createElement("i");
-        newItem.textContent = itemDescription;
+
+        //NEW DESCRIPTION
+        const newDescription = document.createElement("p");
+        newDescription.textContent = inptDescription.value;
+
         newItem.className = "col-md d-flex justify-content-between pt-1";
+
         newIconEdit.className = "bi bi-pencil-square";
         newBtnEdit.appendChild(newIconEdit);
+
         newIconDelete.className = "bi bi-x-square";
         newBtnDelete.appendChild(newIconDelete);
+
+        newItem.appendChild(newDescription);
         newItem.appendChild(newBtnEdit);
         newItem.appendChild(newBtnDelete);
         listTodo.appendChild(newItem);
-
 
         //FUNCTION TO DELETE ITEMS WHEN CLICK IN DELETE BUTTON
         newBtnDelete.addEventListener("click", (e) => {
             e.preventDefault();
             newItem.remove();
             counterItems();
+        });
+
+        //FUNCTION TO EDIT ITEMS WHEN CLICK IN EDIT BUTTON
+        newBtnEdit.addEventListener("click", (e) => {
+            e.preventDefault();
+            newItem.remove();
+            const newDescriptionEdited = document.createElement("input");
+            newDescriptionEdited.value = newItem.textContent;
+
+            newItem.innerHTML = "";
+            newItem.appendChild(newDescriptionEdited);
+
+            //NEW BOTON ADD
+            const newBtnAdd = document.createElement("button");
+
+            //NEW ICON ADD
+            const newIconAdd = document.createElement("i");
+            newIconAdd.className = "bi bi-check-square";
+
+            newBtnAdd.appendChild(newIconAdd);
+            newItem.appendChild(newBtnAdd);
+            listTodo.appendChild(newItem);
         });
         counterItems();
     };
@@ -44,8 +81,8 @@ const itemAdd = () => {
 
 //FUNCTION TO COUNTER ITEMS WHEN IS ADD OR DELETE ITEMS
 const counterItems = () => {
-    let count = document.getElementsByTagName("li");
-    counterList.textContent = count.length;
+    const count_items = document.getElementsByTagName("li");
+    counterList.textContent = count_items.length;
 };
 
 
